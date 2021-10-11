23f471da2fda8a4d87897360f63eaba1e5d60156
valerii <mikolyuk28@gmail.com>
2021-10-11 13:03:40 +0200

    first project

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из просмотреных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),  
+      c = prompt('Один из просмотреных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[b] = d;
+
+console.log(personalMovieDB);
