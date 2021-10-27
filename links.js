4835bd2a2b4be42d74f3e7240126415eb5de2cb2
valerii <mikolyuk28@gmail.com>
2021-10-27 12:39:55 +0200

    DOM

---
+++ links.js
@@ -0,0 +1,80 @@
+"use strict"
+
+let a = 5,
+    b = a;
+
+    b = b + 5;
+
+    console.log(b);
+    console.log(a);
+
+
+    // const obj = {
+    //     a:5,
+    //     b:1
+    // };
+
+    // const copy = obj;
+
+
+    function copy(mainObj) {
+        let objCopy = {};
+
+        let key;
+        for (key in mainObj){
+            objCopy[key] = mainObj [key];
+        }
+
+        return objCopy;
+    }
+
+    const numbers = {
+        a: 2,
+        b: 5,
+        c: {
+            x: 7,
+            y: 4
+        }
+
+    };
+
+    const newNumbers = copy(numbers);
+    newNumbers.a = 10;
+
+    console.log(newNumbers);
+
+
+    const add = {
+        d:17,
+        e:20
+    };
+
+    console.log(Object.assign(numbers, add));
+
+    const clone = Object.assign({}, add);
+
+    clone.d = 20;
+
+
+    // console.log(add);
+    // console.log(clone);
+
+    const oldArray = ['a','b','c'];
+    const newArray = oldArray.slice();
+
+    newArray[1] = ['sgddsfgdfsdf'];
+    console.log(newArray);
+    console.log(oldArray);
+
+
+    const video = ['youtube','vimeo','rutube'],
+            blogs = ['wordpress','licejournal','blogger'],
+            internet = [...video, ...blogs, 'vk', 'facebook'];
+
+            console.log(internet);
+
+
+
+
+
+    
---
+++ practics2.js
@@ -0,0 +1,26 @@
+"use strict"
+// let x = 5; alert( x++ );
+
+// [ ] + false - null + true
+console.log([] + false);   //""false"
+
+//  let y = 1; 
+//  let x = y = 2;
+//   alert(x);
+
+// console.log([ ] + 1 + 2);  //12
+
+// alert( &quot;1&quot;[0] ); //1
+
+// console.log(2 && 1 && null && 0 && undefined);
+
+// И запинаеться на лжи
+// ИЛИ запинаеться на правде
+
+// console.log(!!( a && b ) === (a && b));
+            // 3
+// alert( null || 2 && 3 || 4 );
+
+// const a = [1, 2, 3];  // нет это разные хранилища информации не смотря на то что похожи
+// const b = [1, 2, 3];
+console.log(0 || "" || 2 || undefined || true || falsе); //2
